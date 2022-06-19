e6c8ec6de77a1153ed822b99d9a21b77df48bd71
Pineapple Bun <35952622+asdfg44l@users.noreply.github.com>
2022-06-19 09:17:38 +0800

    Custom hook: to apply patient List/order

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect, useMemo } from 'react'
-import { HTTP_GET } from '../../plugins/axios'
+import { HTTP_GET, HTTP_POST, HTTP_PUT } from '../../plugins/axios'
 import PatientList from './components/List'
 import OrderDialog from './components/OrderDialog'
 
 
-const Home = () => {
+//hook
+//取得患者列表
+function usePatientList() {
     const [patientList, setPatientList] = useState([])
-    const [open, setOpen] = useState(false)
-    const [patientInfo, setPatientInfo] = useState({})
-    const [patientOrder, setPatientOrder] = useState([])
 
     async function getPatientList() {
         try {
@@ -19,15 +18,65 @@ const Home = () => {
         }
     }
 
-    async function getPatientOrder() {
+    return [patientList, getPatientList]
+}
+
+//患者醫囑 CRUD
+function usePatientOrder() {
+    const [patientOrder, setPatientOrder] = useState([])
+
+    //取得患者醫囑
+    async function getPatientOrder(params) {
         try {
-            const patientOrder = await HTTP_GET('/order', { patientId: patientInfo.patientId })
+            const patientOrder = await HTTP_GET('/order', { patientId: params.patientId })
             setPatientOrder(patientOrder)
-            console.log(patientOrder)
         } catch(e) {
             console.error(e)
         }
     }
+
+    //新增醫囑
+    async function addPatientOrder(params) {
+        try {
+            const { patientId, message } = params
+            await HTTP_POST('/order', { patientId, message })
+            getPatientOrder()
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    //修改醫囑
+    async function editPatientOrder(params) {
+        try {
+            const { orderId, message } = params
+            await HTTP_PUT('/order', { orderId, message })
+            getPatientOrder()
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    //刪除醫囑
+    async function deletePatientOrder(params) {
+        try {
+            await HTTP_GET('/order', { orderId: params.orderId })
+            getPatientOrder()
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    return [patientOrder, getPatientOrder, addPatientOrder, editPatientOrder, deletePatientOrder]
+}
+
+
+const Home = () => {
+    const [patientList, getPatientList] = usePatientList()
+    const [patientOrder, getPatientOrder, addPatientOrder, editPatientOrder, deletePatientOrder] = usePatientOrder()
+    const [open, setOpen] = useState(false)
+    const [patientInfo, setPatientInfo] = useState({})
+    
 
     function handleListClick(patientInfo) {
         //after get order
@@ -40,7 +89,7 @@ const Home = () => {
     }, [])
 
     useEffect(() => {
-        getPatientOrder()
+        getPatientOrder(patientInfo)
     }, [patientInfo])
 
     const dialogTitle = useMemo(() => {
